77e5eee3ca58b58a06e1a829f1e3c8caf5ee47b3
ItsNyoty <58821897+ItsNyoty@users.noreply.github.com>
2022-11-12 18:36:41 +0100

    Adding ticket claim interaction

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -127,5 +127,94 @@ module.exports = new Event("interactionCreate", async (client, interaction) => {
         interaction.message.edit({ components: [row] });
       }
     }
+        if (interaction.customId === "ticketlock") {
+      if (interaction.channel.name.startsWith(supportbot.TicketPrefix)) {
+        interaction.message.fetch();
+        let Admin =
+          interaction.guild.roles.cache.find(
+            (AdminUser) => AdminUser.name === supportbot.Admin
+          ) ||
+          interaction.guild.roles.cache.find(
+            (AdminUser) => AdminUser.id === supportbot.Admin
+          );
+        if (!interaction.member.roles.cache.has(Admin.id)) {
+          return await interaction.reply({
+            content: "You don't have permission to do that.",
+            ephemeral: true,
+          });
+        }
+        let all = interaction.channel.permissionOverwrites.cache;
+
+        let parent =
+          interaction.guild.channels.cache.find(
+            (c) => c.name === supportbot.LockTicketCategory
+          ) ||
+          interaction.guild.channels.cache.find(
+            (c) => c.id === supportbot.LockTicketCategory
+          );
+        if (parent) {
+          await interaction.channel.setParent(parent.id);
+        }
+
+        all.forEach((perm) => {
+          interaction.channel.permissionOverwrites.delete(perm.id);
+        });
+        await interaction.channel.permissionOverwrites.create(Admin.id, {
+          VIEW_CHANNEL: true,
+        });
+
+        await interaction.channel.permissionOverwrites.create(
+          interaction.guild.id,
+          {
+            VIEW_CHANNEL: false,
+          }
+        );
+
+        const ticketDeleteButton = new Discord.MessageButton()
+          .setCustomId("ticketclose")
+          .setLabel("Close")
+          .setStyle(supportbot.TicketDeleteColour)
+          .setEmoji(supportbot.TicketDeleteEmoji);
+
+        const row = new Discord.MessageActionRow().addComponents(
+          ticketDeleteButton
+        );
+        const ArchiveEmbed = new Discord.MessageEmbed()
+          .setTitle("Archived")
+          .setDescription(`Archived ${interaction.channel.name}`)
+          .setColor(supportbot.SuccessColour);
+
+        interaction.reply({ embeds: [ArchiveEmbed] });
+        interaction.message.edit({ components: [row] });
+      }
+    }
+    
+    if (interaction.customId === "ticketclaim") {
+      if (interaction.channel.name.startsWith(supportbot.TicketPrefix)) {
+        interaction.message.fetch();
+        let User = interaction.guild.members.cache.get(interaction.user.id);
+        let Admin =
+          interaction.guild.roles.cache.find(
+            (AdminUser) => AdminUser.name === supportbot.Admin
+          ) ||
+          interaction.guild.roles.cache.find(
+            (AdminUser) => AdminUser.id === supportbot.Admin
+          );
+        if (!interaction.member.roles.cache.has(Admin.id)) {
+          return await interaction.reply({
+            content: "You don't have permission to do that.",
+            ephemeral: true,
+          });
+        }
+        let all = interaction.channel.permissionOverwrites.cache;
+ 
+        const ArchiveEmbed = new Discord.MessageEmbed()
+          .setTitle(supportbot.TicketClaimTitle)
+          .setDescription(supportbot.TicketClaimMessage + " <@" + interaction.user.id + ">")
+          .setColor(supportbot.EmbedColour);
+
+        interaction.reply({ embeds: [ArchiveEmbed] });
+      }
+    }
   }
 });
